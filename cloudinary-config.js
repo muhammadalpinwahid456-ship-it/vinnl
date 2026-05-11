1cf79367a489861ed1ec2515104233d1f866d5d1
muhammadalpinwahid456-ship-it <muhammadalpinwahid456@gmail.com>
2026-05-11 13:17:08 +0700

    Add files via upload

---
+++ cloudinary-config.js
@@ -0,0 +1,6 @@
+// Ganti dengan credentials Anda dari Cloudinary Dashboard
+export const CLOUDINARY_CONFIG = {
+    CLOUD_NAME: "YOUR_CLOUD_NAME",
+    UPLOAD_PRESET: "YOUR_UPLOAD_PRESET",
+    API_KEY: "YOUR_API_KEY"
+};
